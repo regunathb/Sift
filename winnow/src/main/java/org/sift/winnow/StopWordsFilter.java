5bb1549058e2852f3e74518c14f497bf526c5941
Regunath B <regunathb@gmail.com>
2013-01-29 12:54:45 +0530

    Added stop words filter as a runtime processor

---
+++ winnow/src/main/java/org/sift/winnow/StopWordsFilter.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2012-2015, the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.sift.winnow;
+
+import org.sift.runtime.Tuple;
+import org.sift.runtime.spi.OutputCollector;
+import org.sift.runtime.spi.Processor;
+
+
+/**
+ * The <code>StopWordsFilter</code> is an implementation of the Sift runtime's {@link Processor} that filters out stop words in the {@link Tuple} data list
+ * 
+ * @author Regunath B
+ * @version 1.0, 28 Jan 2013
+ */
+public class StopWordsFilter implements Processor {
+	
+	/** the StopWords instance */
+	private StopWords stopWords;
+
+	/**
+	 * Interface method implementation. Removes stop words words in the specified Tuple and emits a Tuple without them
+	 * @see org.sift.runtime.spi.Processor#process(org.sift.runtime.Tuple, org.sift.runtime.spi.OutputCollector)
+	 */
+	public void process(Tuple tuple, OutputCollector collector) {
+		Tuple output = new Tuple(tuple.getKey());
+		for (String word : tuple.getValues()) {
+			if (!this.stopWords.isStopWord(word)) {
+				output.addValue(word);
+			}
+		}
+		collector.emit(output);
+	}
+	
+	/** Getter/Setter methods */
+	public StopWords getStopWords() {
+		return this.stopWords;
+	}
+	public void setStopWords(StopWords stopWords) {
+		this.stopWords = stopWords;
+	}	
+
+}
